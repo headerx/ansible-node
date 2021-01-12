42cde65ba4fbac5feb99ab235d0d8cbe261766a3
inmanturbo <inman.turbo@gmail.com>
2021-01-12 12:41:36 -0500

    wip

--- scripts/rancher_launch.sh
+++ scripts/rancher_launch.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 docker run -d --restart=unless-stopped -p 3737:80 -p 3783:443 --privileged -v /opt/rancher:/var/lib/rancher --name=rancher_docker_server rancher/rancher:latest
